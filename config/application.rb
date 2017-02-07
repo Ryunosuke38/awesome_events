139e6056ec33630f46c3de4e55e08be0310197ce
Ryunosuke Murata <ryunosuke77552@gmail.com>
2017-02-07 15:04:38 +0900

    Back To Old Commit And New Commit
    Because I described wrong information and commited.

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
+
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -18,8 +19,7 @@ Bundler.require(*Rails.groups)
 
 module AwesomeEvents
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja
   end
 end
